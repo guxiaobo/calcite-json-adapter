7d03018b5af4554bf2af1bf3df5cafcdd3becab3
Amber <guxiaobo1982@qq.com>
2022-02-23 15:29:31 +0800

    add jsonschematest

---
+++ src/test/java/org/apache/calcite/adapter/json/test/BaseTest.java
@@ -0,0 +1,107 @@
+package org.apache.calcite.adapter.json.test;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.util.Properties;
+
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.schema.Schema;
+
+public class BaseTest {
+	
+	public CalciteConnection openConn1(Schema schema, String schemaName)
+			throws SQLException {
+		Properties info = new Properties();
+		info.setProperty("lex", "JAVA");
+		info.setProperty("caseSensitive", "false");
+		info.setProperty("schema", "hr");
+
+		Connection connection =
+		    DriverManager.getConnection("jdbc:calcite:", info);
+		CalciteConnection conn =
+		    connection.unwrap(CalciteConnection.class);
+		conn.getRootSchema().add(schemaName, schema);
+		return conn;
+	}
+	
+	
+	public static Long exeGetLong(Connection conn, String sql) throws SQLException {
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		Long ret = null;
+		if(rs.next()) {
+			ret = rs.getLong(1);
+		}
+		rs.close();
+		statement.close();
+		return ret;
+	}
+	
+	public static BigDecimal exeGetDecimal(Connection conn, String sql) throws SQLException {
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		BigDecimal ret = null;
+		if(rs.next()) {
+			ret = rs.getBigDecimal(1);
+		}
+		rs.close();
+		statement.close();
+		return ret;
+	}
+	
+	public static Boolean exeGetBoolean(Connection conn, String sql) throws SQLException {
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		Boolean ret = null;
+		if(rs.next()) {
+			ret = rs.getBoolean(1);
+		}
+		rs.close();
+		statement.close();
+		return ret;
+	}
+	
+	public static Date exeGetDate(Connection conn, String sql) throws SQLException {
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		Date ret = null;
+		if(rs.next()) {
+			ret = rs.getDate(1);
+		}
+		rs.close();
+		statement.close();
+		return ret;
+	}
+	
+	public static Time exeGetTime(Connection conn, String sql) throws SQLException {
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		Time ret = null;
+		if(rs.next()) {
+			ret = rs.getTime(1);
+		}
+		rs.close();
+		statement.close();
+		return ret;
+	}
+	
+	public static String exeGetString(Connection conn, String sql) throws SQLException {
+		Statement statement = conn.createStatement();
+		ResultSet rs = statement.executeQuery(sql);
+		String ret = null;
+		if(rs.next()) {
+			ret = rs.getString(1);
+		}
+		rs.close();
+		statement.close();
+		return ret;
+	}
+
+
+}
---
+++ src/test/java/org/apache/calcite/adapter/json/test/JsonSchemaTest.java
@@ -0,0 +1,53 @@
+package org.apache.calcite.adapter.json.test;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.calcite.adapter.json.JsonSchema;
+import org.apache.calcite.jdbc.CalciteConnection;
+import org.apache.calcite.schema.Schema;
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonSchemaTest extends BaseTest{
+	
+	private Map<String, List<JSONObject>> makeJsonMap(){
+		Map<String, List<JSONObject>> map = new HashMap<String, List<JSONObject>>();
+		List<JSONObject> t1 = new ArrayList<JSONObject>();
+		JSONObject r1 = new JSONObject();
+		t1.add(r1);
+		map.put("t1", t1);
+		
+		r1.put("c1", Long.valueOf(100));
+		r1.put("c2", "column2");
+		r1.put("c3", Boolean.FALSE);
+		r1.put("c4", new BigDecimal("2.1"));
+		r1.put("c5", new java.sql.Date(2022, 2, 22));
+		r1.put("c6", new java.sql.Time(System.currentTimeMillis()));
+		r1.put("c7", new java.sql.Timestamp(System.currentTimeMillis()));
+		
+		return map;
+	}
+	
+	@Test
+	public void test10() throws SQLException {
+		Map<String, List<JSONObject>> map = makeJsonMap();
+		
+		String sql1 = "select count(*) from t1";
+		String sql2 = "select count(*) from js.t1";
+
+		Schema schema = new JsonSchema(map);
+		
+		CalciteConnection conn = this.openConn1(schema, "js");		
+		Long l1 = exeGetLong(conn, sql2);
+		System.out.println("sql result1 " + l1);
+		conn.close();
+	}
+
+
+}
